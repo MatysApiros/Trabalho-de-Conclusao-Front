54499cf2af9d2aecfd1839d7ce71d312c3d8c92f
Matheus Canabarro Peña <matheus.canabarro@hotmail.com>
2021-11-16 17:42:38 -0300

    removido firebase

--- trabalho-conclusao-front/src/environments/environment.prod.ts
+++ trabalho-conclusao-front/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://hospitais-porto-alegre-back.herokuapp.com/',
 };
--- trabalho-conclusao-front/src/environments/environment.ts
+++ trabalho-conclusao-front/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://hospitais-porto-alegre-back.herokuapp.com/'
+  baseUrl: 'https://hospitais-porto-alegre-back.herokuapp.com/',
 };
 
 /*
